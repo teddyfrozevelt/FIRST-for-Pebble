0d88e412cb71d4eb2aac62bf9d89e4d2994a741b
Owen D'Aprile <teddyfrozevelt@users.noreply.github.com>
2016-02-19 08:19:04 -0500

    HUGE update
    I have created a webpage that allows users to input information.
    I have also made it so that the team number is saved in the settings of the watch and if the user hasn't selected a team, it is defaulted to 548.
    I am currently running into an issue where it says that I am forbidden to access the api so...

--- src/app.js
+++ src/app.js
@@ -1,52 +1,78 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var Settings = require('settings');
-var currentTime = new Date();
-var year = currentTime.getFullYear();
-var teamNumber = 503;
-var teamURL = 'http://www.thebluealliance.com/api/v2/team/frc' + teamNumber;
+var teamURL;
 Settings.data('teamInfo', {nickname: '', locality: '', region: '', motto: ''});
 var teamInfo = Settings.data('teamInfo');
+
+//Initialize and check if teamNumber has been chosen;
+Settings.data('teamNum');
+var teamNumber = Settings.data('teamNum');
+console.log('teamNumber is: ' + teamNumber);
+if(teamNumber === undefined) {
+  Settings.data('teamNum', 548);
+  teamNumber = Settings.data('teamNum');
+  console.log('teamNumber successfully set to 548');
+  //Retrieve team information and save it
+  ajax ({
+      url: 'www.thebluealliance.com/api/v2/team/frc' + teamNumber,
+      type: 'json',
+      headers: { 'X-TBA-App-Id': 'frc548_owendaprile:FIRST_for_Pebble:v01' }
+    },
+      function(data) {
+        teamInfo.nickname = data.nickname;
+        teamInfo.locality = data.locality;
+        teamInfo.region = data.region;
+        teamInfo.motto = data.motto;
+      },
+      function(error) {
+        console.log('Error retrieving team information: ' + error);
+  })
+  
+}
+console.log('teamNumber is: ' + teamNumber);
+
+//If user opens page, save their response in teamNum
+Pebble.addEventListener('showConfiguration', function(e) {
+  Pebble.openURL('https://testspace-teddyfrozevelt.c9.io/');
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  Settings.data('teamNum', e.response);
+  teamNumber = Settings.data('teamNum');
+  console.log('teamNumber successfully set to ' + teamNumber);
+  //Retrieve team information and save it
+  ajax ({
+    url: 'www.thebluealliance.com/api/v2/team/frc' + teamNumber,
+    type: 'json',
+    headers: { 'X-TBA-App-Id': 'frc548_owendaprile:FIRST_for_Pebble:v01' }
+  },
+      function(data) {
+      teamInfo.nickname = data.nickname;
+      teamInfo.locality = data.locality;
+      teamInfo.region = data.region;
+      teamInfo.motto = data.motto;
+    },
+      function(error) {
+        console.log('Error retrieving team information: ' + error);
+  });
+});
+//Initialize and display UI elements
+
 var mainMenu = new UI.Menu({
   sections: [{
     items: [{
       title: 'Team Info',
       subtitle: 'FRC ' + teamNumber
-    }, {
-      title: 'Events',
-      subtitle: 'For ' + year
-    }, {
-      title: 'Points',
-      subtitle: 'Current Match'
     }]
   }]
 });
 var teamInfoCard = new UI.Card();
 mainMenu.show();
 
-ajax ({
-    url: teamURL,
-    type: 'json',
-    headers: { 'X-TBA-App-Id': 'frc548_owendaprile:FIRST_for_Pebble:v01' }
-  },
-    function(data) {
-      teamInfo.nickname = data.nickname;
-      teamInfo.locality = data.locality;
-      teamInfo.region = data.region;
-      teamInfo.motto = data.motto;
-    },
-    function(error) {
-      console.log('Error retrieving team information: ' + error);
-});
-
 mainMenu.on('select', function(e) {
-  if(e.itemIndex === 0) {
-    teamInfoCard.title(teamInfo.nickname);
-    teamInfoCard.subtitle(teamInfo.locality + ", " + teamInfo.region);
-    teamInfoCard.body(teamInfo.motto);
-    
-    teamInfoCard.show();
-  } else if(e.itemIndex === 1) {
-  } else if(e.itemIndex === 2) {
-  }
+  teamInfoCard.title(teamInfo.nickname);
+  teamInfoCard.subtitle(teamInfo.locality + ", " + teamInfo.region);
+  teamInfoCard.body(teamInfo.motto);
+  teamInfoCard.show();
 });
